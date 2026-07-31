91f27680d4cc3e531e640cddd2677cb29a2089f3
fdg2112 <fdg2112@gmail.com>
2026-07-31 07:26:14 -0300

    Endurecer seguridad: CSP meta + sandbox en iframe del mapa

--- src/components/sections/Lugar.tsx
+++ src/components/sections/Lugar.tsx
@@ -65,6 +65,7 @@ export function Lugar() {
               style={{ border: 0, minHeight: 320 }}
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
+              sandbox="allow-scripts allow-same-origin allow-popups"
               allowFullScreen
             />
           </div>
